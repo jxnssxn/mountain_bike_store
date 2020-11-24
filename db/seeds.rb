2f0e3107d9cf4b9f83eed94f4fcd8182ad40ca34
jimmy <jonssonjimmy8@gmail.com>
2020-11-24 18:46:53 -0500

    initial commit for bike store, started to create bike products in seed file. run db:seed in terminal.

---
+++ db/seeds.rb
@@ -0,0 +1,57 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+1
+Name: "Giant Trance Advanced pro 29"
+Price: 5200
+image_url: "https://images.giant-bicycles.com/b_white,c_pad,h_650,q_80/sptpqtcbccjtghlqjoty/MY20TranceADPRO291_ColorA.jpg"
+
+"https://images.giant-bicycles.com/b_white,c_pad,h_1000,q_60/imudmi6zdmcmfcq1ntue/MY20_TranceAdvPro29_gallery04.jpg"
+
+Description : "The latest iteration of this legendary trail bike marks a huge step forward, offering a true advantage in all-round trail performance. It starts with a full composite frame that’s fitted with our most up-to-date Maestro suspension. The Advanced Forged composite rocker arm and trunnion-mount shock help produce 115mm of smooth rear-wheel travel that’s active and sensitive to small bumps, giving you killer traction and confident control. And the frame geometry is updated with progressive headtube and seattube angles, shorter chainstays and a longer toptube—all engineered to balance the speed and control of its 29-inch wheels with the quickness and agility you need to rail corners, hammer through rock gardens and do it all with confidence and control."
+
+
+
+Name: "Canyon Spectral CFR 9.0"
+Price: 6499.00
+Image_url: "https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dwbb7be757/images/full/full_2020_/2020/full_2020_spectral-cf-r_2234_bk-cr_P5.png?sw=1060&sh=1060&sm=fit&sfrm=png"
+
+"https://scontent.fphl1-2.fna.fbcdn.net/v/t1.0-9/84192512_3014074978625834_167071229927227392_n.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=oetpxC82QVYAX9x3NPc&_nc_ht=scontent.fphl1-2.fna&oh=201f5aef80258b0cbd0d9c47b94c383e&oe=5FE28499"
+
+Description: "The Spectral CFR combines a carbon frame with carbon wheels, cranks, and cockpit to provide a highly desirable trail weapon. Modern geometry, refined Triple Phase Kinematics and carefully considered details result in a breathtakingly beautiful trail bike."
+
+
+
+Name: "Cannondale Habit carbon 1"
+Price: 5500
+Image_url: "https://embed.widencdn.net/img/dorelrl/4ilqyal7nx/2000px@1x/C21_C23201M_Habit_Crb_1_SGY_PD.png"
+
+"https://d1mo5ln9tjltxq.cloudfront.net/-/media/images/my20/bikes/mountain/trail/habit/habit14qa07.jpg?mh=1920&mw=2560&hash=B521F0CCC6EFD6888C8901B849C9345300838CCF"
+
+Description: "A trail bike with its priorities on point: playful agility, progressive design, and killer capability – your favorite trails have never been more fun."
+
+
+
+
+Name: "Santa Cruz Hightower CC X01 Reserve"
+Price: 2999
+Image_url: "https://images.immediate.co.uk/production/volatile/sites/21/2019/12/Santa-Cruz-Hightower-CC-X01-Reserve-01-802d2c4.jpg?webp=true&quality=90&resize=620%2C413"
+
+"https://www.santacruzbicycles.com/files/styles/scb_natural_1440_auto/public/hero/scb_web_product_hightower_orange_herotall.jpg?itok=d8VN9LKS"
+Description: "The Hightower has been a does-it-all-well trail bike since its inception. More maneuverable than its longer-travel sibling, the Megatower, and more at home in the steep-and-chunky than its snappy cousin Tallboy, Hightower excels when the route covers ‘all points in between,’ and descents come courtesy of some big-ass climbs. The VPP suspension’s lower-link-mounted shock creates a nearly linear leverage curve, meaning it mops up bumps of all sizes and maintains the kind of progressivity normally reserved for our V10 DH bike!"
+
+
+Name: "Santa Cruz Megatower"
+Price: 4499
+Image_url: "https://contenderbicycles.com/wp-content/uploads/2019/03/2019-Santa-Cruz-Megatower-Contender-Bicycles.jpg"
+
+"https://images.squarespace-cdn.com/content/v1/5c1896d996d45589e7a4ff7d/1571936489422-CMCQI34TWEF8NEZ71L44/ke17ZwdGBToddI8pDm48kCFTy3UGnhJ-fWvK5PBTMNx7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UQAK6Hrx5oK4v7EgPqT2OFRSRLMHaWtK29l-R4I7CH9pArRUwoSl4wuUs73k4k9WwA/Santa+Cruz+Megatower+CC?format=2500w"
+
+Description: "The Megatower is the fusion of big wheels and the biggest-hitting suspension system. It’s a modern day brawler, as suited to diehard racers as it is to riders wanting to conquer their hometown trails."
+
